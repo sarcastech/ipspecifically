890b08ed4253ab7c1f9901daf83926f34390afaf
s tech <email@sarcas.tech>
2016-12-09 21:01:30 -0500

    move Transform instantiation to withing function call

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict'
 
 const Transform = require('stream').Transform
-const transform = new Transform()
 
 module.exports = function (site) {
+  const transform = new Transform()
   transform._transform = function (chunk, encoding, callback) {
     let ipString = chunk.toString().split('\n').find(function (domain) {
       return domain.match(`^${site}`)
